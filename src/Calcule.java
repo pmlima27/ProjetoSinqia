a571fbbfc2da2adca35e9d862d6491123b9b2730
pmlima27 <patriciamlima27@gmail.com>
2022-10-26 18:46:40 -0300

    adicionando projeto

---
+++ src/Calcule.java
@@ -0,0 +1,11 @@
+import org.jetbrains.annotations.NotNull;
+
+public class Calcule {
+    public static void main(String @NotNull [] args) {
+        Integer valor1 = Integer.valueOf(args[0]);
+        Integer valor2 = Integer.valueOf(args[2]);
+
+        System.out.println(valor1 + valor2);
+
+    }
+}
